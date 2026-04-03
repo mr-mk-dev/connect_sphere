a944e1a7320cf7b2a51b099d635c70065947fb71
mr-mk-dev <itsmenishan.12@gmail.com>
2026-04-04 02:33:52 +0530

    Added Api Respose , Web config , filter chain

--- src/main/java/me/manishcodes/connectsphere/config/SecurityConfig.java
+++ src/main/java/me/manishcodes/connectsphere/config/SecurityConfig.java
@@ -1,8 +1,32 @@
 package me.manishcodes.connectsphere.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http )throws Exception{
+        return http.cors(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public").permitAll()
+                        .anyRequest().authenticated()
+                ).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
 
 }
